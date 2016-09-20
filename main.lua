548806b90f8e64c2d6bdea16b0a5be874405b73a
Salvatore Tropea <tropea05@yahoo.de>
2016-09-20 20:42:03 +0200

    Version in main.lua implemenitert mit Ausgabe auf console

--- main.lua
+++ main.lua
@@ -1,3 +1,22 @@
+-- Developer:   Salvatore Tropea <tropea05@yahoo.de>
+-- Date:        2016-09-20
+
+--Globale table für die Versionierung
+appVersion = {
+  mv = 1,
+  nv = 0,
+  rv = 0,
+  beta = true,
+  
+  printVersion = function (self)
+    print("Version: "..self.mv.."."..self.nv.."."..self.rv)
+    if self.beta then
+      print("Dies ist eine Entwicklungsversion (beta)")
+    end
+  end,
+}
+
+appVersion:printVersion()
 
 print("hallo welt")
 
